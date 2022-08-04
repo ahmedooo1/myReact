de0b48eb8a5440e2af638a612f4f0a3256e3d8c3
ahmedooo1 <aahmadooo997@gmail.com>
2022-08-04 22:52:13 +0200

    note.js

--- my-react/models/Note.js
+++ my-react/models/Note.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 
-
 const NoteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [ true, 'Please add a title'],
-    unique: true,
-    trim: true,
-    maxlength: [40, 'title cannot be more than 40 characters']
-  },
-  description:{
-    type:String,
-    required:true,
-    maxlength:[200, 'Description cannot be more than 200 characters']
-  }
-
+    title: {
+        type: String,
+        required: [true, 'Please add a title'],
+        unique: true,
+        maxlength: [40, 'Title cannot be more than 40 characters']
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: [200, 'Description cannot be more than 200 characters']
+    }
 })
 
-module.exports = mongoose.models.Note;// || mongoose.model('Note','NoteSchema');
+module.exports = mongoose.models.Note || mongoose.model('Note', NoteSchema);
