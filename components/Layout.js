b258a5d35f49e485490359beefc4ccad3249a380
ahmedooo1 <aahmadooo997@gmail.com>
2022-08-08 13:19:10 +0200

    create project

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Navbar from "./Navbar";
+
+
+
+const  Layout = ({ children }) => (
+
+		<>
+			<Head>
+				<title>Ahmad react library App</title>
+			</Head>
+      <Navbar />
+			{children}
+
+</>
+
+)
+
+export default Layout;
