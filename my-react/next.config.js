e4355d5a2cc580fa76795cfd22181e570f536493
ahmedooo1 <aahmadooo997@gmail.com>
2022-08-10 16:02:05 +0200

    edit heroku

--- my-react/next.config.js
+++ my-react/next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
-    MONGO_URI: "mongodb+srv://myReact:myReact@cluster0.eppyd.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URI: "mongodb+srv://myReact:myReact@cluster0.eppyd.mongodb.net/?retryWrites=true&w=majority",
+    "BASE_URL":"https://my-new-appreact.herokuapp.com"
   }
 
 }
